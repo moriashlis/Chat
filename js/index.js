6287503efbfd54065e8a92fa395ec7a75d4fa4dc
moriash <moria2002@gmail.com>
2020-01-11 22:03:38 +0200

    add time to chat

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
 const chat = {};
 chat.contacts = [];
-chat.date = new Date;
 var nameInput = document.querySelector('#name-input')
 // var userText = document.querySelector('#user-text').value;
 chat.text = true;
 chat.start = () => {
     chat.getTheDate();
     chat.createEmoji();
+    // chat.dateTime();
+    // console.log(chat.dateTime());
+    
 }
+chat.dateTime = () => new Date;
 chat.addEmoji = (e) => {
     var newDiv = document.createElement('div');
-    newDiv.classList.add('div-emoji');
+    // newDiv.classList.add('div-emoji');
     document.querySelector('.chat').appendChild(newDiv);
     var addEmojiToChat = document.createElement('img');
-    // addEmojiToChat.classList.add('chat-emoji');
     addEmojiToChat.src = e.target.src;
     newDiv.appendChild(addEmojiToChat);
-    // if (chat.text) {
-    //     addEmojiToChat.classList.add('message1');
-    // }
-    // else {
-    //     addEmojiToChat.classList.add('message2');
-    // }
-    // chat.text = !chat.text;
+    if (chat.text) {
+        newDiv.classList.add('message1');
+    }
+    else {
+        newDiv.classList.add('message2');
+    }
+    chat.text = !chat.text;
 
 }
 chat.createEmoji = () => {
@@ -30,6 +32,7 @@ chat.createEmoji = () => {
         var newEmoji = document.createElement('div');
         document.querySelector('.emoji').appendChild(newEmoji);
         var emoji = document.createElement('img');
+        emoji.style.cursor = 'pointer';
         emoji.addEventListener('click', chat.addEmoji);
         emoji.src = `./images/${i}.png`;
         newEmoji.appendChild(emoji);
@@ -39,7 +42,6 @@ chat.addContact = () => {
     nameInput.style.display = 'block';
 }
 chat.toChat = (e) => {
-
     var contacts = document.querySelectorAll('.contact');
     contacts.forEach(element => {
         element.style.border = 'rgba(70, 161, 75, 0.671) 1px solid'
@@ -49,17 +51,17 @@ chat.toChat = (e) => {
     e.target.style.border = 'rgba(70, 161, 75, 0.671) 3px solid';
     e.target.style.backgroundColor = "rgba(70, 161, 75, 0.671)";
     document.querySelector('#user-text').disabled = '';
-    console.log(e.target);
-
 }
 
-chat.submitContact = () => {    
+chat.submitContact = (e) => {    
     nameInput.style.display = 'none';
     var contact = {};
     contact.text = [];
     contact.firstName = document.querySelector('#first-name').value;
     contact.lastName = document.querySelector('#last-name').value;
     chat.contacts.push(contact);
+    // var x = chat.contacts.indexOf(contact);
+    // console.log(x);
     document.querySelector('#first-name').value = '';
     document.querySelector('#last-name').value = '';
     var newContact = document.createElement('div');
@@ -77,7 +79,8 @@ chat.submitContact = () => {
     newContact.appendChild(nameContact);
 }
 chat.getTheDate = () => {
-    document.querySelector('.time').innerHTML = chat.date.toDateString();
+    var date = chat.dateTime();
+    document.querySelector('.time').innerHTML = date.toDateString();
 }
 chat.submitText = () => {
     var userText = document.querySelector('#user-text').value;
@@ -92,10 +95,16 @@ chat.submitText = () => {
         chat.text = !chat.text;
         newText.innerHTML = userText;
         document.querySelector('.chat').appendChild(newText);
+        var textTime = document.createElement('div');
+        var date = chat.dateTime();
+        textTime.innerHTML = date.toLocaleTimeString();
+        textTime.style.fontSize = '8px';
+        newText.appendChild(textTime);
         chat.contacts[0].text.push(userText);
         document.querySelector('#user-text').value = '';
     }
 }
+
 
 chat.start();
 document.querySelector('.add-contact').addEventListener('click', chat.addContact);
